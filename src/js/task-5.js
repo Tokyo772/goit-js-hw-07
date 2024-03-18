1dd2061f3e412a32e7b2bcfdc1992244692bdf39
Tokyo772 <merokyry33@gmail.com>
2024-03-18 18:33:16 +0100

    task 5

--- src/js/task-5.js
+++ src/js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const buttonEl = bodyEl.querySelector('.change-color');
+const textEl = bodyEl.querySelector('.color');
+
+buttonEl.addEventListener('click', function () {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  textEl.textContent = `${getRandomHexColor()}`;
+});
